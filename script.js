1d774cd0b57d955c11ac595bb7d2641936d738f7
Phuong-Anh-2007 <phuonganhlb1@gmail.com>
2021-09-18 19:33:59 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function blurer() {
-    document.getElementById('game').style.animation='Blur 5s';
-}
+let flag;
 
-// blurer();
+function runBar() {
+    let elem= document.getElementById('runBar');
+    let width=0;
+    let id = setInterval(run,50);
+    function run() {
+        if (width == 100) {
+            clearInterval(id);
+            changeAll();
+        } else {
+            width++;
+            elem.style.width=width+'%';
+            document.getElementById('widthOfBar').innerHTML=width+'%'
+        }
+    }
+}
+runBar();
+
+function changeAll() {
+    let elem = document.getElementById('change');
+    elem.innerHTML='❌ CARO GAME ⭕'
+                    +'<div id="play">Play</div>'
+                    +'<div id="intruc">Intruction</div>';
+}
